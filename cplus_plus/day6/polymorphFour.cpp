c09ba25814069e2d287b95bddd366187d407c51a
cdacchaitali <165935015+cdacchaitali@users.noreply.github.com>
2024-04-18 22:19:32 +0530

    done day6

---
+++ cplus_plus/day6/polymorphFour.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class B
+{
+public:
+	void fun()
+	{
+		cout << "B::fun()" << endl;
+	}
+};
+
+class D
+{
+public:
+	void fun()
+	{
+		cout << "D::fun()" << endl;
+	}
+};
+
+int main()
+{
+	B b;
+	b.fun();
+	D d;
+	d.fun();
+}
---
+++ cplus_plus/day6/polymorphTwo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class B
+{
+public:
+	void fun()
+	{
+		cout << "B::fun()" << endl;
+	}
+};
+
+class D : public B
+{
+public:
+	void fun()
+	{
+		cout << "D::fun()" << endl;
+	}
+};
+
+int main()
+{
+	B b, *bPtr;
+	D d;
+	bPtr = &b;
+	bPtr->fun();
+	bPtr = &d;
+	bPtr->fun();
+}
